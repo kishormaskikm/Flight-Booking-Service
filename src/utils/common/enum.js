a9edf83963b5db9cf16928441894167ffe10c6d4
kishor-km <kishormaski16@gmail.com>
2023-07-16 17:05:26 +0530

    added booking model

---
+++ src/utils/common/enum.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    BUSINESS : 'bUsiness',
+    ECONOMY  :'economy',
+    PRIMIUM_ECONOMY : 'primium-economy',
+    FIRST_CLASS  : 'first-class'
+}
+
+const BOOKING_STATUS = {
+    PENDING   : "pending",
+    CONFIRMED : "confirmed",
+    CANCELLED: "cancelled",
+    INITIATED : 'initiated'
+}
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
